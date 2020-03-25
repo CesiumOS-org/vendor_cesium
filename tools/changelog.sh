7f8016f39b5ed75c58e751b6186663e465bd5c39
svoboda18 <dev.svoboda18@gmail.com>
2020-03-25 14:35:46 +0000

    vendor: tools: re-write changelog generator.
    Signed-off-by: svoboda18 <dev.svoboda18@gmail.com>

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -1,35 +1,54 @@
 #!/bin/sh
+#
+# Copyright (C) 2020 CesuimOS Team
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses
+#
 
-export Changelog=Changelog.txt
+#  VARIABLES
+CHANGELOG=Changelog.txt
 
-if [ -f $Changelog ];
-then
-	rm -f $Changelog
-fi
+# SCRIPT START
+[ -f $CHANGELOG ] && rm -f $CHANGELOG
 
-touch $Changelog
+touch $CHANGELOG
 
 echo "Generating changelog..."
 
 for i in $(seq 14);
 do
-export After_Date=`date --date="$i days ago" +%Y/%m/%d`
-k=$(expr $i - 1)
-	export Until_Date=`date --date="$k days ago" +%Y/%m/%d`
+        b=$(expr $i - 1)
+        DATE=`date --date="$i days ago" +%Y/%m/%d`
+        UDATE=`date --date="$b days ago" +%Y/%m/%d`
 
-	# Line with after --- until was too long for a small ListView
-	echo '=======================' >> $Changelog;
-	echo  "     "$Until_Date       >> $Changelog;
-	echo '=======================' >> $Changelog;
-	echo >> $Changelog;
+        echo "=======================" >> $CHANGELOG
+        echo "      "$DATE       >> $CHANGELOG
+        echo "=======================" >> $CHANGELOG
 
-	# Cycle through every repo to find commits between 2 dates
-	repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date' >> $Changelog
-	echo >> $Changelog;
+        # Cycle through every repo to find commits since $i days ago
+        repo forall -pc "git log --after=$DATE --until=$UDATE --pretty=format:\"change: %h by %an: %s\"" >> $CHANGELOG
+		
+        # New line
+        echo "" >> $CHANGELOG
 done
 
-sed -i 's/project/   */g' $Changelog
+# Trim project in $CHANGELOG
+sed -i 's/project //g' $CHANGELOG
 
-cp $Changelog $OUT/system/etc/
-cp $Changelog $OUT/
-rm -rf $Changelog
+# Ship to $OUT
+cp -r $CHANGELOG $OUT/system/etc/
+cp -r $CHANGELOG $OUT/
+
+# Remove $CHANGELOG
+rm -rf $CHANGELOG
