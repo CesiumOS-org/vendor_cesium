1a29432b55dc558621efffb68df13c7b71693e73
SahilSonar <sss.sonar2003@gmail.com>
2020-05-08 12:31:33 -0500

    vendor: Migrate to LineageOS Updater
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- config/ota.mk
+++ config/ota.mk
@@ -1,12 +1,7 @@
 ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 
-CUSTOM_OTA_VERSION_CODE := ten
-
-CUSTOM_PROPERTIES += \
-    org.cesium.ota.version_code=$(CUSTOM_OTA_VERSION_CODE) \
-
 PRODUCT_PACKAGES += \
-    Updates
+    Updater
 
 PRODUCT_COPY_FILES += \
     vendor/cesium/config/permissions/org.cesium.ota.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.cesium.ota.xml
