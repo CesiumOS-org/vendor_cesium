56f21c8eb999171af1fb99daba206bf75262e8d9
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-05-20 00:41:27 -0300

    vendor: Nuke ota permission
    Move to package repo
    
    Change-Id: Ic7d36a7857eba3352cb62b07dd94ed8464fd09c4
    (cherry picked from commit babdf0a406b2317985f749a802d6e0429d07421d)
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- config/ota.mk
+++ config/ota.mk
@@ -3,7 +3,4 @@ ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
 PRODUCT_PACKAGES += \
     Updater
 
-PRODUCT_COPY_FILES += \
-    vendor/cesium/config/permissions/org.cesium.ota.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.cesium.ota.xml
-
 endif
