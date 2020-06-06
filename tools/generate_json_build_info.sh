6d619c63274f42e4e06bdbb571d151a641104f29
SahilSonar <sss.sonar2003@gmail.com>
2020-06-06 06:25:56 -0500

    vendor: tools: Rework to support new OTA format
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -5,9 +5,8 @@ then
   file_name=$(basename "$file_path")
   if [ -f $file_path ]; then
     file_size=$(stat -c%s $file_path)
-    md5=$(cat "$file_path.md5sum" | cut -d' ' -f1)
+    id=$(cat "$file_path.md5sum" | cut -d' ' -f1)
     datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2);
-    id=$(sha256sum $file_path | awk '{ print $1 }');
-    echo "{\n   \"filename\": \"$file_name\",\n   \"datetime\": $datetime,\n   \"size\": $file_size,\n   \"filehash\": \"$md5\",\n   \"id\": \"$id\"\n}" > $file_path.json
+    echo "{\n   \"filename\": \"$file_name\",\n   \"datetime\": $datetime,\n   \"size\": $file_size,\n   \"id\": \"$id\"\n}" > $file_path.json
   fi
 fi
