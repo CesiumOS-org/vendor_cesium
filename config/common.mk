aef65e36ede2053376df0d119bf664c9a4dafb26
SahilSonar <sss.sonar2003@gmail.com>
2020-06-21 10:10:00 -0500

    [DNM] vendor: Allow whitelisting privileged permissions
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -31,8 +31,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 endif
 
 # Config Center
-PRODUCT_PACKAGES += \
-    ConfigCenter
+#PRODUCT_PACKAGES += \
+#    ConfigCenter
 
 # WallBucket
 PRODUCT_PACKAGES += \
@@ -92,9 +92,15 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/Vendor_045e_Product_0719.kl
 
-# Enforce privapp-permissions whitelist
+# Privapp-permissions whitelist
+ifeq ($(HAS_BROKEN_PRIVAPP), true)
+# Don't enforce priv-app permissions
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.control_privapp_permissions=log
+else
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
+endif
 
 # Power whitelist
 PRODUCT_COPY_FILES += \
