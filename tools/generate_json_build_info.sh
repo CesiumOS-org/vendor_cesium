6e46525baffb42c3108a9bfd72f8b02923c9e914
SahilSonar <sss.sonar2003@gmail.com>
2020-10-18 03:52:00 -0500

    vendor: tools: Fix missing datetime stamp from ota json
    [@SahilSonar | CesiumOS]
    
    - Make use of ./out/target/product/$DEVICE/ instead of $OUT
    
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -3,10 +3,11 @@ if [ "$1" ]
 then
   file_path=$1
   file_name=$(basename "$file_path")
+  DEVICE=$(echo $TARGET_PRODUCT | cut -d "_" -f2)
   if [ -f $file_path ]; then
     file_size=$(stat -c%s $file_path)
     id=$(cat "$file_path.md5sum" | cut -d' ' -f1)
-    datetime=$(grep ro\.build\.date\.utc $OUT/system/build.prop | cut -d= -f2);
+    datetime=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
     echo "{\n   \"filename\": \"$file_name\",\n   \"datetime\": $datetime,\n   \"size\": $file_size,\n   \"id\": \"$id\"\n}" > $file_path.json
   fi
 fi
