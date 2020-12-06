RevengeOS/android_vendor_revengeos

envsetup: Define our ROM for SmartBuild-enabled device trees

build/envsetup.sh
@@ -791,3 +791,7 @@ function xda_push(){(
 
 # Allow GCC 4.9
 export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
+
+# Define our ROM for SmartBuild-enabled device trees
+export SMARTBUILD_RELEASE=revengeos
+

CesiumOS-org/vendor_cesium

Change-Id: I214020202106db1d61b6c8278d628055f39fd962

build/envsetup.sh
@@ -595,3 +595,7 @@ function fixup_common_out_dir() {
         mkdir -p ${common_out_dir}
     fi
 }
+
+# Define our ROM for SmartBuild-enabled device trees
+export SMARTBUILD_RELEASE=cesium
+