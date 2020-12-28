a379d66b3bd1d19475e82ecded356dc84cb71960
Alessandro Astone <ales.astone@gmail.com>
2020-12-28 12:34:21 +0100

    vendor: More checks for copying AB backuptool scripts
    Pixels tree have moved AB_OTA_PARTITIONS to BoardConfig.mk,
    which is unavailable when parsing config/common.mk
    In order to still copy AB backuptool scripts for them, check for
    AB_OTA_POSTINSTALL_CONFIG too which still lives in device.mk
    
    Change-Id: I871e2595bc4f083db6491de0d9574bae2794e654
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -58,7 +58,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.ota.allow_downgrade=true
 endif
 
-ifneq ($(AB_OTA_PARTITIONS),)
+ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
     vendor/cesium/prebuilt/common/bin/backuptool_ab.sh:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.sh \
     vendor/cesium/prebuilt/common/bin/backuptool_ab.functions:$(TARGET_COPY_OUT_SYSTEM)/bin/backuptool_ab.functions \
