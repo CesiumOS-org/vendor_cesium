045b2505f4e43fe4e3f56cef8a8c7b67886339c1
SahilSonar <sss.sonar2003@gmail.com>
2021-02-13 09:31:55 +0100

    vendor: config: Include fwk-detect only on QCOM devices
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -5,6 +5,10 @@ include vendor/cesium/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/cesium/config/BoardConfigQcom.mk
+# Namespace for fwk-detect
+TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
+PRODUCT_SOONG_NAMESPACES += \
+    $(TARGET_FWK_DETECT_PATH)/fwk-detect
 endif
 
 include vendor/cesium/config/BoardConfigSoong.mk
@@ -14,8 +18,3 @@ PRODUCT_SOONG_NAMESPACES += \
     frameworks/av/camera/cameraserver \
     frameworks/av/services/camera/libcameraservice
 endif
-
-# Namespace for fwk-detect
-TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
-PRODUCT_SOONG_NAMESPACES += \
-    $(TARGET_FWK_DETECT_PATH)/fwk-detect
