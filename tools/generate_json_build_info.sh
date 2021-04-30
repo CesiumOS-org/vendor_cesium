88e2a4827ec2e291354c5af07865eb4c2452f195
Victor <victor@corvusrom.com>
2021-04-30 21:46:35 +0300

    generate_json_build_info: refactor for our ota app

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -8,6 +8,8 @@ then
     file_size=$(stat -c%s $file_path)
     id=$(cat "$file_path.md5sum" | cut -d' ' -f1)
     datetime=$(grep ro\.build\.date\.utc ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
-    echo "{\n   \"filename\": \"$file_name\",\n   \"datetime\": $datetime,\n   \"size\": $file_size,\n   \"id\": \"$id\"\n}" > $file_path.json
+    cesium_version=$(grep org\.cesium\.build_version ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2 | cut -d "v" -f2);
+    custom_build_type=$(grep org\.cesium\.build_type ./out/target/product/$DEVICE/system/build.prop | cut -d= -f2);
+    echo "{\n  \"response\": [\n    {\n      \"datetime\": $datetime,\n      \"filename\": \"$file_name\",\n      \"id\": \"$id\",\n      \"romtype\": \"$custom_build_type\",\n      \"size\": $file_size,\n      \"url\": \"https://sourceforge.net/projects/cesiumos-org/files/$DEVICE/$file_name\",\n      \"version\": \"$cesium_version\"\n    }\n  ]\n}" > $file_path.json
   fi
 fi
